9044ef7427d759b6bd0e315b9e98618cdb7498a9
LIN TING <Ting005@users.noreply.github.com>
2023-02-13 14:39:02 +0800

    Add files via upload

---
+++ src/dataloaders/ASP_DICT.py
@@ -0,0 +1,4 @@
+LAPTOP_ASP_DICT = {'laptop': 0, 'support': 1, 'display': 2, 'battery': 3, 'company': 4, 'software': 5,
+                   'keyboard': 6, 'os': 7, 'mouse': 8, 'multimedia_devices': 9, 'graphics': 10, 'cpu': 11, 'memory': 12, 'warranty': 13}
+REST14_ASP_DICT = {'food': 0, 'price': 1, 'service': 2, 'ambience': 3, 'anecdotes/miscellaneous': 4}
+MAMS_AC = {'food': 0, 'place': 1, 'staff': 2, 'miscellaneous': 3, 'service': 4, 'price': 5, 'menu': 6, 'ambience': 7}
